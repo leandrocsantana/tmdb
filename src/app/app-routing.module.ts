b47257c80d16118d02ea24b79b6c3d7087940eac
Leandro Brum <leandrocsantana@yahoo.com.br>
2020-06-15 08:18:08 -0300

    Criado rotas home,login,favoritos e detalhe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+import { DetalheComponent } from './components/detalhe/detalhe.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'favoritos', component: FavoritosComponent },
+  { path: 'detalhe', component: DetalheComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+import { DetalheComponent } from './components/detalhe/detalhe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    FavoritosComponent,
+    DetalheComponent
   ],
   imports: [
     BrowserModule,
